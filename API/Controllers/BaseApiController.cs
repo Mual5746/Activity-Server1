5796791061ccdb4b24c0f72dd99fd7913367b808
Max Ali <maalxb@syntronic.com>
2023-01-31 22:20:26 +0100

    Addde somfiles into API

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("[api/activities]")]
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
