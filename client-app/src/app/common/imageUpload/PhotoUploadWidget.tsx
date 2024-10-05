32463df77abd0c6b228b806a706fca45bd65621f
Max Ali <mohseni716015@gmail.com>
2024-10-05 15:26:20 +0200

    added styling for dropzone and added react cropper 201-202

--- client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,21 +1,51 @@
-import { Grid, Header } from "semantic-ui-react";
+import { Button, Grid, Header } from "semantic-ui-react";
 import PhotoUploadWidgetDropzone from "./PhotoWidgetDropzone";
+import { useEffect, useState } from "react";
+import PhotoWidgetCropper from "./PhotoWidgetCropper";
 
 
 export default function PhotoUploadWidget (){
+    const [files, setFiles] = useState<any>([]);
+    const [cropper, setCropper] = useState<Cropper>();
+
+    function onCrop() {
+        if (cropper) {
+            cropper.getCroppedCanvas().toBlob(blob => console.log(blob!))
+        }
+    }
+
+    useEffect(() => {
+        return () => {
+            files.forEach((file: any) => URL.revokeObjectURL(file.preview))
+        }
+    }, [files]);
+
     return (
         < Grid>
             <Grid.Column width={4}>
                 < Header sub color="teal" content="step 1 Add " />
-                <PhotoUploadWidgetDropzone />
+                <PhotoUploadWidgetDropzone setFiles={setFiles} />
             </Grid.Column>
             <Grid.Column width={1}/>
             <Grid.Column width={4}>
                 < Header sub color="teal" content="step 2 Add " />
+                {files && files.length > 0 &&
+                        <PhotoWidgetCropper setCropper={setCropper} imagePreview={files[0].preview} />
+                    }
             </Grid.Column>
             <Grid.Column width={1}/>
             <Grid.Column width={4}>
                 < Header sub color="teal" content="step 3 Add " />
+                { files && files.length > 0 &&
+                <>
+                 <div className="img-preview" style={{ minHeight: 200, overflow: 'hidden' }} />
+                <Button.Group widths={2}>
+                        <Button  onClick={onCrop} positive icon='check' />
+                        <Button  onClick={() => setFiles([])} icon='close' />
+                    </Button.Group>
+                </>
+                
+                }
             </Grid.Column>
         </Grid>
     )
--- client-app/src/app/common/imageUpload/PhotoWidgetDropzone.tsx
+++ client-app/src/app/common/imageUpload/PhotoWidgetDropzone.tsx
@@ -1,21 +1,37 @@
 import React, {useCallback} from 'react'
 import {useDropzone} from 'react-dropzone'
+import { Header, Icon } from 'semantic-ui-react';
 
-export default function PhotowidgetDropzone() {
+interface Props {
+    setFiles : (files: any) => void;
+}
+
+export default function PhotowidgetDropzone({setFiles}: Props) {
+    const dzStyles = {
+        border: 'dashed 3px #eee',
+        borderColor: '#eee',
+        borderRadius: '5px',
+        paddingTop: '30px',
+        textAlign: 'center' as 'center',
+        height: '200px'
+    }
+    const dzActive = {
+        borderColor: 'green',
+    }
+
   const onDrop = useCallback((acceptedFiles: any) => {
-    // Do something with the files
-    console.log(acceptedFiles);
-  }, [])
+    setFiles(acceptedFiles.map((file: any) => Object.assign(file, {
+        preview: URL.createObjectURL(file)
+    })))
+}, [setFiles])
+
   const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-      }
+    <div {...getRootProps()} style={isDragActive ? {...dzStyles, ...dzActive} : dzStyles}>
+        <input {...getInputProps()} />
+        <Icon name='upload' size='huge' />
+        <Header content='Drop image here' />
     </div>
-  )
+)
 }
