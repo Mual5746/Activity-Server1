681e31ca2f4a238e0a54450c68516bf3eda28eca
Max Ali <mohseni716015@gmail.com>
2024-10-10 12:17:48 +0200

    228 -227 folowing property to the maping configuration and updating the other handlers with the following property

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,10 +1,10 @@
 ﻿using Application.Core;
+using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities
@@ -20,10 +20,11 @@ namespace Application.Activities
         {
             private readonly DataContext _context;
             public readonly IMapper _mapper;
+            private readonly IUserAccessor _userAccessor;
 
-            
-            public Handler(DataContext context, IMapper mapper ) 
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _mapper = mapper;
                 _context = context;
             }
@@ -32,8 +33,8 @@ namespace Application.Activities
             public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var activities = await _context.Activities
-                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
-                    .ToListAsync(cancellationToken);
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider, new{currentUsername = _userAccessor.GetUsername()})
+                    .ToListAsync();
 
                 return Result<List<ActivityDto>>.Success(activities);
             }
