7d83e867ce17e382612e9ddca6a56e0ce810604c
Max Ali <mohseni716015@gmail.com>
2024-10-13 23:28:35 +0200

    added method to follow and unfollow, added the button component for follow

--- client-app/src/Features/profiles/ProfileCard.tsx
+++ client-app/src/Features/profiles/ProfileCard.tsx
@@ -3,6 +3,7 @@ import { Card, Icon, Image } from "semantic-ui-react";
 import { Profile } from "../../app/models/profile";
 import { observer } from "mobx-react-lite";
 import { Link } from "react-router-dom";
+import FollowButton from './FollowButton';
 interface Props {
     profile: Profile
 }
@@ -25,6 +26,7 @@ export default observer(function ProfileCard({ profile }: Props) {
                 <Icon name='user' />
                 {profile.followersCount} Followers
             </Card.Content>
+            <FollowButton profile={profile} />
         </Card>
     )
 })
