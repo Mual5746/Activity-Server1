b1490a1e3ab3df6d79b0ff347dff453515f1194e
Max Ali <mohseni716015@gmail.com>
2024-10-16 13:51:49 +0200

    uppdated react app to react vite and also prepered cleant for production 253

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,7 +4,6 @@ import 'react-calendar/dist/Calendar.css';
 import './app/layout/styles.css';
 import "react-datepicker/dist/react-datepicker.css";
 import 'react-toastify/dist/ReactToastify.min.css';
-import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
 import { router } from './app/router/Routes';
 import { RouterProvider } from 'react-router-dom';
@@ -18,8 +17,3 @@ root.render(
   </StoreContext.Provider>,
     
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
